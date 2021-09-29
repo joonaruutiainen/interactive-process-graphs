6f7315be136593eb540dcc719fe239d1d01a34e0
Joona Ruutiainen <joona.ruutiainen@tuni.fi>
2021-09-29 17:16:01 +0300

    added simple dropdown selection to display example processes in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProcessGraph from './components/ProcessGraph';
+import exampleProcesses from './exampleProcesses';
 
-const App: React.FC = () => <ProcessGraph nodes={['node1', 'node2', 'node3']} />;
+const App: React.FC = () => {
+  const [selectedProcess, setSelectedProcess] = useState(exampleProcesses[0]);
 
+  return (
+    <div>
+      <select
+        defaultValue={exampleProcesses[0].name}
+        onChange={e => {
+          e.preventDefault();
+          const process = exampleProcesses.find(p => p.name === e.target.value);
+          if (process) setSelectedProcess(process);
+        }}
+      >
+        {exampleProcesses.map(process => (
+          <option value={process.name} key={process.name}>
+            {process.name}
+          </option>
+        ))}
+      </select>
+      <ProcessGraph nodes={selectedProcess.nodes.map(nodes => nodes.description)} />
+    </div>
+  );
+};
 export default App;
