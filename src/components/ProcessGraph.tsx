ecaf1a3e2ce576524826a6a0bd9ef77258487d71
Tuomas Knaapi <tuomas.knaapi@tuni.fi>
2021-10-06 16:44:31 +0300

    View is movable and zoomable with buttons and mouse

--- src/components/ProcessGraph.tsx
+++ src/components/ProcessGraph.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
+
+const ZoomButtons = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  max-width: calc(100vw - 60px);
+  margin-bottom: 10px;
+  button {
+    margin-left: 10px;
+    width: 2em;
+  }
+`;
 
 const Container = styled.div`
   display: flex;
@@ -19,11 +32,30 @@ interface Props {
 }
 
 const ProcessGraph: React.FC<Props> = ({ nodes }) => (
-  <Container>
-    {nodes.map(node => (
-      <ExampleItem key={node}>{node}</ExampleItem>
-    ))}
-  </Container>
+  <TransformWrapper initialScale={1} minScale={0.8} maxScale={10}>
+    {({ zoomIn, zoomOut, resetTransform }) => (
+      <>
+        <ZoomButtons>
+          <button type='button' onClick={() => zoomIn()}>
+            +
+          </button>
+          <button type='button' onClick={() => zoomOut()}>
+            -
+          </button>
+          <button type='button' onClick={() => resetTransform()}>
+            x
+          </button>
+        </ZoomButtons>
+        <TransformComponent>
+          <Container>
+            {nodes.map(node => (
+              <ExampleItem key={node}>{node}</ExampleItem>
+            ))}
+          </Container>
+        </TransformComponent>
+      </>
+    )}
+  </TransformWrapper>
 );
 
 export default ProcessGraph;
