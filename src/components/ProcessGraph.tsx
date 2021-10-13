70cd49788653ba3c94846c9a0a3b19b4425fb85e
Tuomas Knaapi <tuomas.knaapi@tuni.fi>
2021-10-13 15:21:52 +0300

    Zoomable and moveable feature for ProcessGraph component

--- src/components/ProcessGraph.tsx
+++ src/components/ProcessGraph.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Canvas, EdgeData, NodeData } from 'reaflow';
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
 import { Edge } from '../types/Edge';
 import { Node } from '../types/Node';
+
+const ZoomButtons = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  max-width: calc(100vw - 60px);
+  margin-bottom: 10px;
+  button {
+    margin-left: 10px;
+    width: 2em;
+  }
+`;
 
 interface ProcessGraphProps {
   nodes: Node[];
@@ -17,7 +31,33 @@ const ProcessGraph: React.FC<ProcessGraphProps> = ({ nodes, edges }) => {
     to: edge.to.toString(),
   }));
   return (
-    <Canvas width={1800} height={800} nodes={nodeData} edges={edgeData} layoutOptions={{ 'elk.direction': 'RIGHT' }} />
+    <TransformWrapper wheel={{ step: 0.1 }} initialScale={1} minScale={0.8} maxScale={10}>
+      {({ zoomIn, zoomOut, resetTransform }) => (
+        <>
+          <ZoomButtons>
+            <button type='button' onClick={() => zoomIn()}>
+              +
+            </button>
+            <button type='button' onClick={() => zoomOut()}>
+              -
+            </button>
+            <button type='button' onClick={() => resetTransform()}>
+              x
+            </button>
+          </ZoomButtons>
+          <TransformComponent>
+            <Canvas
+              zoomable={false}
+              width={1800}
+              height={800}
+              nodes={nodeData}
+              edges={edgeData}
+              layoutOptions={{ 'elk.direction': 'RIGHT' }}
+            />
+          </TransformComponent>
+        </>
+      )}
+    </TransformWrapper>
   );
 };
 export default ProcessGraph;
