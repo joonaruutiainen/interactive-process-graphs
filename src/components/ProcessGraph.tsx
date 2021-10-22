53fe83d426875654eec49a83d0cca836a5efdf85
Tuomas Knaapi <tuomas.knaapi@tuni.fi>
2021-10-22 16:12:10 +0300

    Zoomable and movable ProvessGraph component

--- src/components/ProcessGraph.tsx
+++ src/components/ProcessGraph.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
 import { Canvas, EdgeData, NodeData } from 'reaflow';
 import { Edge } from '../types/Edge';
 import { Node } from '../types/Node';
+import useWindowDimensions from '../hooks/useWindowDimensions';
+
+const Container = styled.div`
+  background-color: lightgrey;
+  cursor: grab;
+  &:active {
+    cursor: grabbing;
+  }
+`;
+
+const ZoomButtons = styled.div`
+  display: flex;
+  width: 100%;
+  max-width: calc(100vw - 60px);
+  margin-bottom: 10px;
+  button {
+    margin-left: 10px;
+    width: 2em;
+  }
+`;
 
 interface ProcessGraphProps {
   nodes: Node[];
@@ -16,22 +38,47 @@ const ProcessGraph: React.FC<ProcessGraphProps> = ({ nodes, edges }) => {
     from: edge.from.toString(),
     to: edge.to.toString(),
   }));
+
+  const { width, height } = useWindowDimensions();
   return (
-    <Canvas
-      readonly
-      nodes={nodeData}
-      edges={edgeData}
-      layoutOptions={{
-        'elk.direction': 'RIGHT',
-        'org.eclipse.elk.layered.nodePlacement.strategy': 'NETWORK_SIMPLEX',
-        'org.eclipse.elk.layered.nodePlacement.bk.fixedAlignment': 'BALANCED',
-        'org.eclipse.elk.layered.nodePlacement.bk.edgeStraightening': 'IMPROVE_STRAIGHTNESS',
-        'org.eclipse.elk.edgeRouting': 'ORTHOGONAL',
-        'org.eclipse.elk.layered.layering.strategy': 'NETWORK_SIMPLEX',
-        'org.eclipse.elk.layered.nodePlacement.favorStraightEdges': 'true',
-        'org.eclipse.elk.layered.crossingMinimization.strategy': 'LAYER_SWEEP',
-      }}
-    />
+    <Container>
+      <TransformWrapper wheel={{ step: 0.1 }} minScale={0.8} maxScale={10}>
+        {({ zoomIn, zoomOut, resetTransform }) => (
+          <>
+            <ZoomButtons>
+              <button type='button' onClick={() => zoomIn()}>
+                +
+              </button>
+              <button type='button' onClick={() => zoomOut()}>
+                -
+              </button>
+              <button type='button' onClick={() => resetTransform()}>
+                x
+              </button>
+            </ZoomButtons>
+            <TransformComponent>
+              <Canvas
+                readonly
+                maxWidth={width * 0.9}
+                maxHeight={height * 0.9}
+                nodes={nodeData}
+                edges={edgeData}
+                layoutOptions={{
+                  'elk.direction': 'RIGHT',
+                  'org.eclipse.elk.layered.nodePlacement.strategy': 'NETWORK_SIMPLEX',
+                  'org.eclipse.elk.layered.nodePlacement.bk.fixedAlignment': 'BALANCED',
+                  'org.eclipse.elk.layered.nodePlacement.bk.edgeStraightening': 'IMPROVE_STRAIGHTNESS',
+                  'org.eclipse.elk.edgeRouting': 'ORTHOGONAL',
+                  'org.eclipse.elk.layered.layering.strategy': 'NETWORK_SIMPLEX',
+                  'org.eclipse.elk.layered.nodePlacement.favorStraightEdges': 'true',
+                  'org.eclipse.elk.layered.crossingMinimization.strategy': 'LAYER_SWEEP',
+                }}
+              />
+            </TransformComponent>
+          </>
+        )}
+      </TransformWrapper>
+    </Container>
   );
 };
 export default ProcessGraph;
