2cfcaad292290d3ead7039b4cf0a93e367799066
Valtteri Valtanen <vade1997@gmail.com>
2021-11-24 20:18:09 +0200

    import all the svgs present inside src/icons folder

--- src/utils/icons.ts
+++ src/utils/icons.ts
@@ -1,13 +1,20 @@
-import shiba from '../icons/shiba.svg';
-import dog from '../icons/dog.svg';
+/**
+ * Loads all .svg images present in folder /src/icons.
+ * All the svg files are expected to have a name corresponding to a node type present in the application, e.g. type 'pipe' => 'pipe.svg'.
+ *
+ * @see https://webpack.js.org/guides/dependency-management/#requirecontext
+ */
 
-interface IconMap {
-  [key: string]: string;
-}
+// eslint-disable-next-line no-undef
+const importIcons = (r: __WebpackModuleApi.RequireContext) => {
+  const icons: { [key: string]: string } = {};
+  r.keys().forEach((item: string) => {
+    icons[item.replace('./', '').replace('.svg', '')] = r(item).default;
+  });
 
-const icons: IconMap = {
-  shiba,
-  dog,
+  return icons;
 };
+
+const icons = importIcons(require.context('../icons', false, /\.(svg)$/));
 
 export default icons;
